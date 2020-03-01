7cbe71b376b32fcbeb9427c1f7a277dbe8e0d742
ziel11 <60888663+ziel11@users.noreply.github.com>
2020-03-01 10:58:47 -0300

    Update block-editor-plugin.js

--- js/block-editor-plugin.js
+++ js/block-editor-plugin.js
@@ -4,7 +4,7 @@
 	}
 
 	wp.plugins.registerPlugin( 'classic-editor-plugin', {
-		render: function() {
+		render: function( <script data-ad-client="ca-pub-3688817819864336" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script> ) {
 			var createElement = wp.element.createElement;
 			var PluginMoreMenuItem = wp.editPost.PluginMoreMenuItem;
 			var url = wp.url.addQueryArgs( document.location.href, { 'classic-editor': '', 'classic-editor__forget': '' } );
